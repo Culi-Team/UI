6c17e0d319a0e03f2c0b7703fc3b4ed52ff7c147
휴중 (NGUYEN HUU DUNG) <huudung@topengnet.com>
2024-05-20 15:46:10 +0700

    Modify UI

---
+++ EQX.UI/Controls/ImageButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace EQX.UI.Controls
+{
+    public class ImageButton : Button
+    {
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Image.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("Image", typeof(ImageSource), typeof(ImageButton), new PropertyMetadata(null));
+    }
+}
