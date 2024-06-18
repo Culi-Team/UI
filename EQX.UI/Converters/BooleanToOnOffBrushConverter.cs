7605a68eee3f5acc8022cb2aaec91d11e2ea3c47
휴중 (NGUYEN HUU DUNG) <huudung@topengnet.com>
2024-06-18 21:14:45 +0700

    Add DInputView

---
+++ EQX.UI/Converters/BooleanToOnOffBrushConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace EQX.UI.Converters
+{
+    internal class BooleanToOnOffBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool onOff = (bool)value;
+
+            return onOff ? Brushes.Blue : Brushes.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
