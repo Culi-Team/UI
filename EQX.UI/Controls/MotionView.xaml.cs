b9801bd7c0479155d1f6aedad7141c27b861a217
휴중 (NGUYEN HUU DUNG) <huudung@topengnet.com>
2024-06-21 18:33:58 +0700

    EQX.Motion MotionView bug fix

--- EQX.UI/Controls/MotionView.xaml.cs
+++ EQX.UI/Controls/MotionView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using EQX.Motion;
+﻿using EQX.Core.Motion;
+using EQX.Motion;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,51 +22,126 @@ namespace EQX.UI.Controls
     /// </summary>
     public partial class MotionView : UserControl
     {
+        private List<string> jogSpeedList = new List<string>
+        {
+            "SuperSlow",
+            "Slow",
+            "Medium",
+            "High",
+        };
+
+        private double jogSpeedRate = 5;
+
+        private List<double> jogSpeedRates = new List<double>
+        {
+            .03,
+            .10,
+            .20,
+            .40,
+        };
+
+        private List<double> absDistanceList = new List<double>
+        {
+            0.001,
+            0.010,
+            0.1,
+            1,
+            10,
+        };
+
         public MotionView()
         {
             InitializeComponent();
-            cbBoxStepInc.ItemsSource = new List<int> { 1, 2, 5, 10 };
-            IncMode.IsChecked = true;
+            cbBoxStepInc.ItemsSource = jogSpeedList;
         }
 
         private void MoveDec_ButtonDown(object sender, MouseButtonEventArgs e)
         {
+            if (IsDataContextValid() == false) return;
+
             if(JogMode.IsChecked == true) 
             {
-                (this.DataContext as MotionBase).MoveJog(2000, false);
+                ((IMotion)DataContext).MoveJog(
+                    ((IMotion)DataContext).Parameter.MaxVelocity * jogSpeedRate,
+                    false);
             }
             else
             {
-                (this.DataContext as MotionBase).MoveInc(-(double)cbBoxStepInc.SelectedItem, 2000);
+                // Move INC by 10% max speed
+                ((IMotion)DataContext).MoveInc((double)cbBoxStepInc.SelectedItem * -1,
+                   ((IMotion)DataContext).Parameter.MaxVelocity * .10);
             }
         }
 
-        private void MoveDec_ButtonUp(object sender, MouseButtonEventArgs e)
+        private void MoveButton_ButtonUp(object sender, MouseButtonEventArgs e)
         {
+            if (IsDataContextValid() == false) return;
+
             if (JogMode.IsChecked == true)
             {
-                (this.DataContext as MotionBase).Stop();
+                ((IMotion)DataContext).Stop();
             }
         }
 
         private void MoveInc_ButtonDown(object sender, MouseButtonEventArgs e)
         {
+            if (IsDataContextValid() == false) return;
+
             if (JogMode.IsChecked == true)
             {
-                (this.DataContext as MotionBase).MoveJog(2000, true);
+                ((IMotion)DataContext).MoveJog(
+                    ((IMotion)DataContext).Parameter.MaxVelocity * jogSpeedRate,
+                    true);
             }
             else
             {
-                (this.DataContext as MotionBase).MoveInc((double)cbBoxStepInc.SelectedItem, 2000);
+                // Move INC by 10% max speed
+                ((IMotion)DataContext).MoveInc((double)cbBoxStepInc.SelectedItem,
+                    ((IMotion)DataContext).Parameter.MaxVelocity * .10);
             }
         }
 
-        private void MoveInc_ButtonUp(object sender, MouseButtonEventArgs e)
+        private bool IsDataContextValid()
         {
-            if (JogMode.IsChecked == true)
+            if (DataContext == null) return false;
+            if (DataContext.GetType().GetInterfaces().Contains(typeof(IMotion)) == false) return false;
+
+            return true;
+        }
+
+        private void JogAbsMode_Checked(object sender, RoutedEventArgs e)
+        {
+            cbBoxStepInc.ItemsSource = JogMode.IsChecked == true ? jogSpeedList : absDistanceList;
+            cbBoxStepInc.SelectedIndex = 0;
+        }
+
+        private void cbBoxStepInc_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (JogMode.IsChecked == false) return;
+            if (cbBoxStepInc.SelectedIndex < 0) cbBoxStepInc.SelectedIndex = 0;
+
+            jogSpeedRate = jogSpeedRates[cbBoxStepInc.SelectedIndex];
+        }
+
+        private void ButtonServoOn_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsDataContextValid() == false) return;
+
+            if (((IMotion)DataContext).Status.IsMotionOn)
             {
-                (this.DataContext as MotionBase).Stop();
+                ((IMotion)DataContext).MotionOff();
             }
+            else
+            {
+                ((IMotion)DataContext).MotionOn();
+            }
+        }
+
+        private void ButtonOrigin_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsDataContextValid() == false) return;
+
+            ((IMotion)DataContext).SearchOrigin();
         }
     }
 }
