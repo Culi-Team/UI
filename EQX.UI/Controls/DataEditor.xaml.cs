306644e282878e285a76c03349c626989a8a4163
Xuanhv21 <xuanhoangvinh01@gmail.com>
2024-07-18 15:27:08 +0700

    Add Recipe UI

---
+++ EQX.UI/Controls/DataEditor.xaml.cs
@@ -0,0 +1,333 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using EQX.Core.Recipe;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EQX.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for DataEditor.xaml
+    /// </summary>
+    public partial class DataEditor : Window, INotifyPropertyChanged
+    {
+        public SingleRecipeMinMaxAttribute SingleRecipeMinMax { get; set; }
+        public DataEditor(double value, SingleRecipeMinMaxAttribute singleRecipeMinMax)
+        {
+            NewValue = value;
+            SingleRecipeMinMax = singleRecipeMinMax;
+            InitializeComponent();
+            this.DataContext = this;
+            Calculator = new Calculation(Add);
+        }
+        private string inputString = "";
+        public string InputString
+        {
+            get { return inputString; }
+            set
+            {
+                inputString = value;
+                OnPropertyChanged("InputString");
+            }
+        }
+
+        #region Caculation delegate
+        private double number1;
+        public double Number1
+        {
+            get { return number1; }
+            set
+            {
+                number1 = value;
+                if (Calculator != null)
+                {
+                    NewValue = Calculator(Number1, Number2);
+                }
+                OnPropertyChanged("Number1");
+                OnPropertyChanged("NewValue");
+            }
+        }
+
+        private double number2;
+        public double Number2
+        {
+            get { return number2; }
+            set
+            {
+                number2 = value;
+                if (Calculator != null)
+                {
+                    NewValue = Calculator(Number1, Number2);
+                }
+                OnPropertyChanged("Number2");
+                OnPropertyChanged("NewValue");
+            }
+        }
+        private double newValue;
+        public double NewValue
+        {
+            get { return newValue; }
+            set
+            {
+                newValue = double.Parse(string.Format("{0:0.000}", value));
+                if (SingleRecipeMinMax != null)
+                {
+                    if (newValue < SingleRecipeMinMax.Min) newValue = SingleRecipeMinMax.Min;
+
+                    if (newValue > SingleRecipeMinMax.Max) newValue = SingleRecipeMinMax.Max;
+                }
+                OnPropertyChanged("NewValue");
+            }
+        }
+        public delegate double Calculation(double num1, double num2);
+
+        private Calculation calculator;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public Calculation Calculator
+        {
+            get { return calculator; }
+            set
+            {
+                calculator = value;
+                if (Calculator != null)
+                {
+                    NewValue = Calculator(Number1, Number2);
+                }
+                OnPropertyChanged("Caculator");
+                OnPropertyChanged("NewValue");
+            }
+        }
+
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Sub(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Muliple(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Division(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        #endregion
+
+        private void BigButton_Click(object sender, RoutedEventArgs e)
+        {
+            string tag = (sender as Button).Tag.ToString();
+
+            switch (tag)
+            {
+                case "ApplyButton":
+                    DialogResult = true;
+                    this.Close();
+                    break;
+                case "CloseButton":
+                    DialogResult = false;
+                    this.Close();
+                    break;
+                case "ClearButton":
+                    InputString = "";
+                    Number2 = 0;
+
+                    Calculator = new Calculation(Add);
+                    break;
+                case "BackButton":
+                    if (InputString.Length > 1)
+                    {
+                        InputString = InputString.Remove(InputString.Length - 1, 1);
+
+                        GetNumbersFromInputString();
+                    }
+                    else if (InputString.Length == 1)
+                    {
+                        InputString = InputString.Remove(InputString.Length - 1, 1);
+
+                        Number1 = 0;
+                        Number2 = 0;
+                    }
+                    else
+                    {
+                        Number1 = 0;
+                        Number2 = 0;
+                    }
+                    break;
+            }
+        }
+
+        private void HeaderLabel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void OperandButton_Click(object sender, RoutedEventArgs e)
+        {
+            string operand = (sender as Button).Content.ToString();
+
+            if (string.IsNullOrEmpty(InputString))
+            {
+                InputString += Number1.ToString();
+            }
+            else if (InputString.Contains("+") || InputString.Contains("–") || InputString.Contains("*") || InputString.Contains("/"))
+            {
+                if (InputString.EndsWith("+") || InputString.EndsWith("–") || InputString.EndsWith("*") || InputString.EndsWith("/"))
+                {
+                    InputString = InputString.Remove(InputString.Length - 1, 1);
+                }
+                else
+                {
+                    InputString = NewValue.ToString();
+                    Number1 = NewValue;
+                    Number2 = 0;
+                }
+            }
+
+            InputString += operand;
+
+            switch (operand)
+            {
+                case "+":
+                    Calculator = new Calculation(Add);
+                    break;
+
+                case "–":
+                    Calculator = new Calculation(Sub);
+                    break;
+
+                case "*":
+                    Calculator = new Calculation(Muliple);
+                    break;
+
+                case "/":
+                    Calculator = new Calculation(Division);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            GetNumbersFromInputString((sender as Button).Content.ToString());
+        }
+
+        private void SignButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Calculator == Muliple || Calculator == Division)
+            {
+                Number2 = 1;
+            }
+            InputString = NewValue.ToString();
+            Number1 = NewValue;
+            Number2 = 0;
+            Calculator = new Calculation(Add);
+
+            if (InputString.StartsWith("-"))
+            {
+                InputString = InputString.Remove(0, 1);
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(InputString))
+                {
+                    InputString = "-" + Number1.ToString();
+                }
+                else
+                {
+                    InputString = "-" + InputString;
+                }
+            }
+
+            GetNumbersFromInputString();
+        }
+
+        private void GetNumbersFromInputString(string insertingText = null)
+        {
+            try
+            {
+                if (InputString.Contains("+") || InputString.Contains("–") || InputString.Contains("*") || InputString.Contains("/"))
+                {
+                    if (InputString.EndsWith("+") || InputString.EndsWith("–") || InputString.EndsWith("*") || InputString.EndsWith("/"))
+                    {
+                        Number1 = double.Parse(InputString.Remove(InputString.Length - 1, 1));
+                    }
+
+                    if (insertingText != null)
+                    {
+                        InputString += insertingText;
+                    }
+
+                    int operandIndex = 3 + InputString.IndexOf("+") + InputString.IndexOf("–") + InputString.IndexOf("*") + InputString.IndexOf("/");
+
+                    string number2Str = InputString.Substring(operandIndex + 1, InputString.Length - operandIndex - 1);
+
+                    if (number2Str == ".")
+                    {
+                        Number2 = 0;
+                    }
+                    else
+                    {
+                        Number2 = double.Parse(number2Str);
+                    }
+                }
+                else
+                {
+                    if (insertingText != null)
+                    {
+                        InputString += insertingText;
+                    }
+
+                    if (InputString == ".")
+                    {
+                        Number1 = 0;
+                    }
+                    else
+                    {
+                        Number1 = double.Parse(InputString);
+                    }
+                    Number2 = 0;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Wrong input format");
+                DialogResult = false;
+                Close();
+            }
+        }
+
+        private void CurrentValue_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+        }
+    }
+
+}
---
+++ EQX.UI/Controls/SingleRecipe.xaml.cs
@@ -0,0 +1,66 @@
+﻿using EQX.Core.Recipe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EQX.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for SingleRecipe.xaml
+    /// </summary>
+    public partial class SingleRecipe : UserControl
+    {
+        public bool IsHeader
+        {
+            get { return (bool)GetValue(IsHeaderProperty); }
+            set { SetValue(IsHeaderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsHeader.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsHeaderProperty =
+            DependencyProperty.Register("IsHeader", typeof(bool), typeof(SingleRecipe), new PropertyMetadata(false));
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Value.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(double), typeof(SingleRecipe), new PropertyMetadata(0.0));
+
+        public SingleRecipe(SingleRecipeDescriptionAttribute singleRecipeDescription,
+            SingleRecipeMinMaxAttribute singleRecipeMinMax)
+        {
+            InitializeComponent();
+
+            SingleRecipeDescription = singleRecipeDescription;
+            SingleRecipeMinMax = singleRecipeMinMax;
+            this.DataContext = this;
+        }
+
+        public SingleRecipeDescriptionAttribute SingleRecipeDescription { get; set; }
+        public SingleRecipeMinMaxAttribute SingleRecipeMinMax { get; set; }
+
+        private void Value_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            DataEditor dataEditor = new DataEditor(Value, SingleRecipeMinMax);
+            if(dataEditor.ShowDialog() == true)
+            {
+                Value = dataEditor.NewValue;
+            }
+        }
+    }
+}
