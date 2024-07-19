5c882d8a4c304a74fbe4ac18df25c56eeb7a2cf1
휴중 (NGUYEN HUU DUNG) <nguyenhuudung.1310@gmail.com>
2024-07-19 20:13:41 +0700

    Update DInputView DOutputView

--- EQX.UI/Converters/BooleanToOnOffBrushConverter.cs
+++ EQX.UI/Converters/BooleanToOnOffBrushConverter.cs
@@ -1,16 +1,35 @@
-﻿using System.Globalization;
+﻿using Newtonsoft.Json.Linq;
+using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media;
 
 namespace EQX.UI.Converters
 {
+    internal class InOutStatusToOnOffBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is bool bValue && values[1] is string strName)
+            {
+                if (strName.ToUpper().StartsWith("SPARE")) return Brushes.DarkGray;
+                return bValue ? Brushes.Green : Brushes.Tomato;
+            }
+            return Binding.DoNothing;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
     internal class BooleanToOnOffBrushConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if(value is bool bValue)
             {
-                return bValue ? Brushes.Green : Brushes.Red;
+                return bValue ? Brushes.Green : Brushes.Tomato;
             }
             return Binding.DoNothing;
         }
