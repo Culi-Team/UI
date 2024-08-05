34ce6f0ea973129bf0561c04063a520a648d85d0
NGUYỄN HỮU DŨNG <nguyenhuudung.1310@gmail.com>
2024-08-05 09:22:02 +0700

    Login / Control UI modify

---
+++ EQX.UI/Converters/AddSpacesToSentenceConverter.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace EQX.UI.Converters
+{
+    public class AddSpacesToSentenceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string strValue)
+            {
+                return AddSpacesToSentence(strValue);
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+
+        private string AddSpacesToSentence(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return "";
+            StringBuilder newText = new StringBuilder(text.Length * 2);
+            newText.Append(text[0]);
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (char.IsUpper(text[i]) && text[i - 1] != ' ')
+                    newText.Append(' ');
+                newText.Append(text[i]);
+            }
+            return newText.ToString();
+        }
+    }
+}
