10cb7f31ce6c610bbcbd9c632bfeed07e6ae4b38
hieu202 <hieubvc159@gmail.com>
2024-08-09 21:48:13 +0700

    Add IO Connect

---
+++ EQX.UI/Resources/Converters/BoolToBrushConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace UTGAutoLoadUnload.Converters
+{
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isActive = (bool)value;
+            return isActive ? Brushes.Lime : Brushes.Tomato; 
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
