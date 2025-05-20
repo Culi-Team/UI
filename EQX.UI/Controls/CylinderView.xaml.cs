2f1ab04fc057d16f57f9971289c0db112627193f
hieu202 <hieubvc159@gmail.com>
2025-05-20 16:48:12 +0700

    Add CylynderView Interlock

--- EQX.UI/Controls/CylinderView.xaml.cs
+++ EQX.UI/Controls/CylinderView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using EQX.InOut;
+﻿using EQX.Core.InOut;
+using EQX.Core.Motion;
+using EQX.InOut;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,11 +27,33 @@ namespace EQX.UI.Controls
         {
             InitializeComponent();
         }
+        public ICylinder Cylinder
+        {
+            get { return (ICylinder)GetValue(CylinderProperty); }
+            set { SetValue(CylinderProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for Motion.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CylinderProperty =
+            DependencyProperty.Register("Cylinder", typeof(ICylinder), typeof(CylinderView), new PropertyMetadata(null));
 
+        public bool Interlock
+        {
+            get { return (bool)GetValue(InterlockProperty); }
+            set { SetValue(InterlockProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for Interlock.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty InterlockProperty =
+            DependencyProperty.Register("Interlock", typeof(bool), typeof(CylinderView), new PropertyMetadata(true));
         private void ForwardButton_Click(object sender, RoutedEventArgs e)
         {
+
             if (this.DataContext is CylinderBase cylinder)
             {
+                if (Interlock == false)
+                {
+                    MessageBoxEx.ShowDialog("Interlock Check Error");
+                    return;
+                }
                 if (cylinder.CylinderType == Core.InOut.ECylinderType.ForwardBackwardReverse ||
                     cylinder.CylinderType == Core.InOut.ECylinderType.UpDownReverse ||
                     cylinder.CylinderType == Core.InOut.ECylinderType.RightLeftReverse ||
@@ -41,15 +65,20 @@ namespace EQX.UI.Controls
                     cylinder.Backward();
                     return;
                 }
-
                 cylinder.Forward();
             }
         }
 
         private void BackwardButton_Click(object sender, RoutedEventArgs e)
         {
+
             if (this.DataContext is CylinderBase cylinder)
             {
+                if (Interlock == false)
+                {
+                    MessageBoxEx.ShowDialog("Interlock Check Error");
+                    return;
+                }
                 if (cylinder.CylinderType == Core.InOut.ECylinderType.ForwardBackwardReverse ||
                     cylinder.CylinderType == Core.InOut.ECylinderType.UpDownReverse ||
                     cylinder.CylinderType == Core.InOut.ECylinderType.RightLeftReverse ||
