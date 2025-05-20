1ac74a0c7bafce013616f04b31d48ef772462098
Xuanhv21 <xuanhoangvinh01@gmail.com>
2025-05-20 08:55:44 +0700

    Add CustomRollingFileAppender Log

---
+++ EQX.UI/CustomRollingFileAppender.cs
@@ -0,0 +1,113 @@
+﻿using log4net.Appender;
+using log4net.Core;
+using System.IO;
+using System.Windows.Documents;
+
+namespace EQX.UI
+{
+    public class CustomRollingFileAppender : RollingFileAppender
+    {
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            if (File != null && Layout.Header != null)
+            {
+                string fileContent = System.IO.File.ReadAllText(File);
+                string header = Layout.Header;
+
+                if (fileContent.EndsWith(header))
+                {
+                    base.Append(loggingEvent);
+                    WriteFooter();
+                }
+                else
+                {
+                    RemoveExistingFooter();
+                    base.Append(loggingEvent);
+                    WriteFooter();
+                }
+            }
+
+        }
+
+        protected override void Append(LoggingEvent[] loggingEvents)
+        {
+            if (File != null && Layout.Header != null)
+            {
+
+                string fileContent = System.IO.File.ReadAllText(File);
+                string header = Layout.Header;
+
+                if (fileContent.EndsWith(header))
+                {
+                    base.Append(loggingEvents);
+                    WriteFooter();
+                }
+                else
+                {
+                    RemoveExistingFooter();
+                    base.Append(loggingEvents);
+                    WriteFooter();
+                }
+            }
+
+        }
+
+        private void WriteFooter()
+        {
+            if (File != null && Layout.Footer != null)
+            {
+                RemoveExistingFooter();
+                lock (this)
+                {
+                    using (var stream = new FileStream(File, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
+                    using (var writer = new StreamWriter(stream))
+                    {
+                        writer.Write(Layout.Footer);
+                    }
+                }
+            }
+        }
+
+        private void RemoveExistingFooter()
+        {
+            if (File != null && Layout.Footer != null)
+            {
+                string fileContent = System.IO.File.ReadAllText(File);
+                string footer = Layout.Footer;
+                if (fileContent.EndsWith(footer))
+                {
+                    fileContent = fileContent.Substring(0, fileContent.Length - footer.Length);
+                    System.IO.File.WriteAllText(File, fileContent);
+                }
+            }
+        }
+
+        protected override void WriteHeader()
+        {
+            if (Layout is not null && QuietWriter is not null && !QuietWriter.Closed)
+            {
+                if (File == null) return;
+
+                if (Layout.Header is string h)
+                {
+                    string fileContent = System.IO.File.ReadAllText(File);
+                    if (fileContent.Contains(h)) return;
+
+                    lock (this)
+                    {
+                        using (var stream = new FileStream(File, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
+                        using (var writer = new StreamWriter(stream))
+                        {
+                            writer.Write(h);
+                        }
+                    }
+                }
+            }
+        }
+
+        protected override void WriteFooterAndCloseWriter()
+        {
+            CloseWriter();
+        }
+    }
+}
