4ab3a8dcbba8687888c53df62ad07e487a326c51
Xuanhv21 <xuanhoangvinh01@gmail.com>
2025-08-22 13:33:12 +0700

    Add Main UI Image Button Samsung

--- EQX.UI/Controls/ImageButton.cs
+++ EQX.UI/Controls/ImageButton.cs
@@ -39,6 +39,42 @@ namespace EQX.UI.Controls
 
     }
 
+    public class ImageRadioButtonV2 : RadioButton
+    {
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CornerRadius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(ImageRadioButtonV2), new PropertyMetadata(new CornerRadius(0)));
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Image.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("Image", typeof(ImageSource), typeof(ImageRadioButtonV2), new PropertyMetadata(null));
+
+
+        public ImageSource DisabledImage
+        {
+            get { return (ImageSource)GetValue(DisabledImageProperty); }
+            set { SetValue(DisabledImageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for DisabledImage.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DisabledImageProperty =
+            DependencyProperty.Register("DisabledImage", typeof(ImageSource), typeof(ImageRadioButtonV2), new PropertyMetadata(null));
+
+    }
+
+
     public class ImageButton : Button
     {
         public CornerRadius CornerRadius
