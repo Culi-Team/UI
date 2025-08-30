168d3f6fe5c85407f1767e871a9ae41f828cae3d
NGUYEN PHU TRONG <trongong123@gmail.com>
2025-08-30 16:38:13 +0700

    Update UI

---
+++ EQX.UI/Controls/RunModeDialog.xaml.cs
@@ -0,0 +1,51 @@
+﻿using EQX.Core.Process;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EQX.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for RunModeDialog.xaml
+    /// </summary>
+    public partial class RunModeDialog : Window
+    {
+        public RunModeDialog()
+        {
+            InitializeComponent();
+            if (DataContext is RunModeDialogViewModel vm)
+            {
+                vm.RequestClose += () =>
+                {
+                    DialogResult = true;
+                    SelectedMode = vm.SelectedMode;
+                    Close();
+                };
+            }
+        }
+        public EMachineRunMode SelectedMode { get; private set; }
+
+        public static EMachineRunMode? ShowRunModeDialog()
+        {
+            return Application.Current.Dispatcher.Invoke(() =>
+            {
+                var dialog = new RunModeDialog();
+                var owner = Application.Current.Windows.OfType<Window>().FirstOrDefault(w => w.IsActive);
+                if (owner != null)
+                    dialog.Owner = owner;
+                return dialog.ShowDialog() == true ? dialog.SelectedMode : (EMachineRunMode?)null;
+            });
+        }
+    }
+}
---
+++ EQX.UI/Controls/RunModeDialogViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using EQX.Core.Process;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.UI.Controls
+{
+    public partial class RunModeDialogViewModel
+    {
+        public RunModeDialogViewModel()
+        {
+            SelectModeCommand = new RelayCommand<EMachineRunMode>(mode =>
+            {
+                SelectedMode = mode;
+                RequestClose?.Invoke();
+            });
+            CloseCommand = new RelayCommand(() => RequestClose?.Invoke());
+        }
+
+        public EMachineRunMode SelectedMode { get; private set; }
+
+        public IRelayCommand<EMachineRunMode> SelectModeCommand { get; }
+
+        public IRelayCommand CloseCommand { get; }
+
+        public event Action? RequestClose;
+    }
+}
