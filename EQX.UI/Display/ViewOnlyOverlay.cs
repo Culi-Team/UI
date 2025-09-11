4da36545bb9f37599631185815b3dd7c13fb6034
NGUYEN PHU TRONG <trongong123@gmail.com>
2025-09-11 13:59:48 +0700

    Add 2 Screen View Option

---
+++ EQX.UI/Display/ViewOnlyOverlay.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EQX.UI.Display
+{
+    public class ViewOnlyOverlay
+    {
+        private ViewOnlyOverlayWindow? _overlay;
+
+        public void ShowOn(int monitorIndex)
+        {
+            if (_overlay != null)
+                return;
+
+            var monitors = EnumerateMonitors();
+            if (monitorIndex < 0 || monitorIndex >= monitors.Count)
+                return;
+
+            var target = monitors[monitorIndex];
+            _overlay = new ViewOnlyOverlayWindow
+            {
+                Left = target.Left,
+                Top = target.Top,
+                Width = target.Width,
+                Height = target.Height
+            };
+            _overlay.Show();
+        }
+
+        public void Hide()
+        {
+            _overlay?.Close();
+            _overlay = null;
+        }
+
+        private static IList<MonitorInfo> EnumerateMonitors()
+        {
+            var result = new List<MonitorInfo>();
+            EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero,
+                (IntPtr hMonitor, IntPtr hdc, ref RECT rect, IntPtr data) =>
+                {
+                    result.Add(new MonitorInfo(rect.Left,
+                                               rect.Top,
+                                               rect.Right - rect.Left,
+                                               rect.Bottom - rect.Top));
+                    return true;
+                }, IntPtr.Zero);
+            return result;
+        }
+
+        private record MonitorInfo(int Left, int Top, int Width, int Height);
+
+        private delegate bool MonitorEnumProc(IntPtr hMonitor, IntPtr hdc, ref RECT lprcMonitor, IntPtr dwData);
+
+        [DllImport("user32.dll")]
+        private static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip, MonitorEnumProc lpfnEnum, IntPtr dwData);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+    }
+}
