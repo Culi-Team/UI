bba6ab9d6d5c3e18ae575bba824fc3a7cf2268f0
NGUYEN PHU TRONG <trongong123@gmail.com>
2025-09-24 14:00:01 +0700

    Add Alarm, Warning Service

---
+++ EQX.UI/Converters/RectangleConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace EQX.UI.Converters
+{
+    public class RectangleConverter : JsonConverter<Rectangle>
+    {
+        public override Rectangle Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return JsonSerializer.Deserialize<Rectangle>(ref reader, options);
+        }
+
+        public override void Write(Utf8JsonWriter writer, Rectangle value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+            writer.WriteNumber("X", value.X);
+            writer.WriteNumber("Y", value.Y);
+            writer.WriteNumber("Width", value.Width);
+            writer.WriteNumber("Height", value.Height);
+            writer.WriteEndObject();
+        }
+    }
+}
