b9dbd1b2ab2d6bb6a7a9ad63838734e28d4f3217
NGUYEN PHU TRONG <trongong123@gmail.com>
2025-12-05 11:32:59 +0700

    Update CylinderView

--- EQX.UI/Controls/CylinderView.xaml.cs
+++ EQX.UI/Controls/CylinderView.xaml.cs
@@ -31,59 +31,71 @@ namespace EQX.UI.Controls
         // Using a DependencyProperty as the backing store for Interlock.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty InterlockProperty =
             DependencyProperty.Register("Interlock", typeof(bool), typeof(CylinderView), new PropertyMetadata(true));
-        private void ForwardButton_Click(object sender, RoutedEventArgs e)
+        private async void ForwardButton_Click(object sender, RoutedEventArgs e)
         {
-
-            if (this.DataContext is CylinderBase cylinder)
+            if (DataContext is CylinderBase cylinder == false)
             {
-                if (Interlock == false)
-                {
-                    MessageBoxEx.ShowDialog("Interlock Check Error");
-                    return;
-                }
-                if (cylinder.CylinderType == Core.InOut.ECylinderType.ForwardBackwardReverse ||
-                    cylinder.CylinderType == Core.InOut.ECylinderType.UpDownReverse ||
-                    cylinder.CylinderType == Core.InOut.ECylinderType.RightLeftReverse ||
-                    cylinder.CylinderType == Core.InOut.ECylinderType.LockUnlockReverse ||
-                    cylinder.CylinderType == Core.InOut.ECylinderType.GripUngripReverse ||
-                    cylinder.CylinderType == Core.InOut.ECylinderType.AlignUnalignReverse ||
-                    cylinder.CylinderType == Core.InOut.ECylinderType.FlipUnflipReverse ||
-                    cylinder.CylinderType == Core.InOut.ECylinderType.ClampUnclampReverse
-                    )
-                {
-                    cylinder.Backward();
-                    return;
-                }
-                cylinder.Forward();
+                return;
             }
+
+            if (!IsInterlockSatisfied(cylinder))
+            {
+                return;
+            }
+
+            var timeout = TimeSpan.FromSeconds(cylinder.ForwardTimeoutSeconds);
+            if (IsReverseCylinder(cylinder))
+            {
+                await cylinder.MoveBackwardWithTimeoutAsync(timeout);
+                return;
+            }
+
+            await cylinder.MoveForwardWithTimeoutAsync(timeout);
         }
 
-        private void BackwardButton_Click(object sender, RoutedEventArgs e)
+        private async void BackwardButton_Click(object sender, RoutedEventArgs e)
         {
-
-            if (this.DataContext is CylinderBase cylinder)
+            if (DataContext is CylinderBase cylinder == false)
             {
-                if (Interlock == false)
-                {
-                    MessageBoxEx.ShowDialog("Interlock Check Error");
-                    return;
-                }
-                if (cylinder.CylinderType == Core.InOut.ECylinderType.ForwardBackwardReverse ||
-                    cylinder.CylinderType == Core.InOut.ECylinderType.UpDownReverse ||
-                    cylinder.CylinderType == Core.InOut.ECylinderType.RightLeftReverse ||
-                    cylinder.CylinderType == Core.InOut.ECylinderType.LockUnlockReverse ||
-                    cylinder.CylinderType == Core.InOut.ECylinderType.GripUngripReverse ||
-                    cylinder.CylinderType == Core.InOut.ECylinderType.AlignUnalignReverse ||
-                    cylinder.CylinderType == Core.InOut.ECylinderType.FlipUnflipReverse ||
-                    cylinder.CylinderType == Core.InOut.ECylinderType.ClampUnclampReverse
-                    )
-                {
-                    cylinder.Forward();
-                    return;
-                }
-
-                cylinder.Backward();
+                return;
             }
+
+            if (!IsInterlockSatisfied(cylinder))
+            {
+                return;
+            }
+
+            var timeout = TimeSpan.FromSeconds(cylinder.BackwardTimeoutSeconds);
+            if (IsReverseCylinder(cylinder))
+            {
+                await cylinder.MoveForwardWithTimeoutAsync(timeout);
+                return;
+            }
+
+            await cylinder.MoveBackwardWithTimeoutAsync(timeout);
+        }
+
+        private bool IsInterlockSatisfied(CylinderBase cylinder)
+        {
+            if (!Interlock || !cylinder.IsInterlockSatisfiedState)
+            {
+                MessageBoxEx.ShowDialog("Interlock Check Error");
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsReverseCylinder(CylinderBase cylinder)
+        {
+            return cylinder.CylinderType == Core.InOut.ECylinderType.ForwardBackwardReverse ||
+                   cylinder.CylinderType == Core.InOut.ECylinderType.UpDownReverse ||
+                   cylinder.CylinderType == Core.InOut.ECylinderType.RightLeftReverse ||
+                   cylinder.CylinderType == Core.InOut.ECylinderType.LockUnlockReverse ||
+                   cylinder.CylinderType == Core.InOut.ECylinderType.GripUngripReverse ||
+                   cylinder.CylinderType == Core.InOut.ECylinderType.AlignUnalignReverse ||
+                   cylinder.CylinderType == Core.InOut.ECylinderType.FlipUnflipReverse ||
+                   cylinder.CylinderType == Core.InOut.ECylinderType.ClampUnclampReverse;
         }
     }
 }
---
+++ EQX.UI/Converters/BooleanAndConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EQX.UI.Converters
+{
+    public class BooleanAndConverter : IMultiValueConverter
+    {
+        public object Convert(object[]? values, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (values == null || values.Length == 0)
+            {
+                return false;
+            }
+
+            foreach (var value in values)
+            {
+                if (value is bool boolValue)
+                {
+                    if (!boolValue)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public object[] ConvertBack(object? value, Type[] targetTypes, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
