aaeb6d912bf3b54086a07b29e7e00da6636bb53f
NGUYEN PHU TRONG <trongong123@gmail.com>
2025-12-09 13:24:19 +0700

    Big Update AlertView

--- EQX.UI/Controls/AlertNotifyView.xaml.cs
+++ EQX.UI/Controls/AlertNotifyView.xaml.cs
@@ -1,21 +1,60 @@
-﻿using EQX.Core.Common;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Input;
+using EQX.Core.Common;
 
 namespace EQX.UI.Controls
 {
     /// <summary>
     /// Interaction logic for AlertNotifyView.xaml
     /// </summary>
-    public partial class AlertNotifyView : Window
+    public partial class AlertNotifyView : Window, INotifyPropertyChanged
     {
-        public AlertModel AlertModel { get; set; }
+        private AlertModel _alertModel;
+        private string _selectedLanguage;
+        private List<string> _availableLanguages = new();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public AlertModel AlertModel
+        {
+            get => _alertModel;
+            set
+            {
+                _alertModel = value;
+                OnPropertyChanged();
+            }
+        }
+
         public bool IsWarning { get; set; }
+        public IAlertService? AlertService { get; set; }
+
+        public List<string> AvailableLanguages
+        {
+            get => _availableLanguages;
+            private set
+            {
+                _availableLanguages = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SelectedLanguage
+        {
+            get => _selectedLanguage;
+            set
+            {
+                _selectedLanguage = value;
+                OnPropertyChanged();
+                OnLanguageChanged();
+            }
+        }
 
         public AlertNotifyView()
         {
             InitializeComponent();
-            this.DataContext = this;
+            DataContext = this;
         }
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -24,8 +63,13 @@ namespace EQX.UI.Controls
                 DragMove();
             }
         }
-        
-        public static bool? ShowDialog(AlertModel alarmModel, bool isWarning = false)
+
+        public static bool? ShowDialog(IAlertService alertService, int alertId, bool isWarning = false)
+        {
+            return ShowDialog(alertService.GetById(alertId), isWarning, alertService);
+        }
+
+        public static bool? ShowDialog(AlertModel alarmModel, bool isWarning = false, IAlertService? alertService = null)
         {
             return Application.Current.Dispatcher.Invoke(() =>
             {
@@ -44,13 +88,35 @@ namespace EQX.UI.Controls
                 {
                     IsWarning = isWarning,
                     AlertModel = alarmModel,
+                    AlertService = alertService,
                     Width = width * 0.6,
                     Height = height * 0.6,
                 };
 
+                messageBoxEx.InitializeLanguages();
+
                 messageBoxEx.ShowDialog();
                 return messageBoxEx.DialogResult;
             });
+        }
+
+        private void InitializeLanguages()
+        {
+            var languageList = AlertService?.SupportedCultures?.ToList() ?? new List<string> { "English", "Vietnamese" };
+            AvailableLanguages = languageList;
+            SelectedLanguage = AlertService?.CurrentCulture ?? languageList.FirstOrDefault();
+        }
+
+        private void OnLanguageChanged()
+        {
+            if (AlertService == null || AlertModel == null || string.IsNullOrWhiteSpace(SelectedLanguage))
+            {
+                return;
+            }
+
+            AlertService.ChangeCulture(SelectedLanguage);
+            AlertModel = AlertService.GetById(AlertModel.Id);
+            OnPropertyChanged(nameof(AlertModel));
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
@@ -63,6 +129,11 @@ namespace EQX.UI.Controls
             {
                 Close();
             }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
--- EQX.UI/Converters/RectangleConverter.cs
+++ EQX.UI/Converters/RectangleConverter.cs
@@ -8,7 +8,27 @@ namespace EQX.UI.Converters
     {
         public override Rectangle Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            return JsonSerializer.Deserialize<Rectangle>(ref reader, options);
+            using var document = JsonDocument.ParseValue(ref reader);
+            var root = document.RootElement;
+
+            int x = GetInt32OrDefault(root, "X");
+            int y = GetInt32OrDefault(root, "Y");
+            int width = GetInt32OrDefault(root, "Width");
+            int height = GetInt32OrDefault(root, "Height");
+
+            if (root.TryGetProperty("Location", out var location))
+            {
+                x = GetInt32OrDefault(location, "X");
+                y = GetInt32OrDefault(location, "Y");
+            }
+
+            if (root.TryGetProperty("Size", out var size))
+            {
+                width = GetInt32OrDefault(size, "Width");
+                height = GetInt32OrDefault(size, "Height");
+            }
+
+            return new Rectangle(x, y, width, height);
         }
 
         public override void Write(Utf8JsonWriter writer, Rectangle value, JsonSerializerOptions options)
@@ -19,6 +39,16 @@ namespace EQX.UI.Converters
             writer.WriteNumber("Width", value.Width);
             writer.WriteNumber("Height", value.Height);
             writer.WriteEndObject();
+        }
+
+        private static int GetInt32OrDefault(JsonElement element, string propertyName)
+        {
+            if (element.TryGetProperty(propertyName, out var property) && property.TryGetInt32(out var value))
+            {
+                return value;
+            }
+
+            return 0;
         }
     }
 }
